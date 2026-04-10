8390bb0e7a772afbd99b12709b9de29a85a8d650
nogakeren <nogakeren@gmail.com>
2026-04-10 17:53:59 +0300

    shuffle using np all the way until the end where use torch.from_numpy

--- records/track_10min_16mb/2026-04-09_liger_SP8192_3LayerRecur_ParResid_QK525_LegalTTT/train_gpt.py
+++ records/track_10min_16mb/2026-04-09_liger_SP8192_3LayerRecur_ParResid_QK525_LegalTTT/train_gpt.py
@@ -64,13 +64,19 @@ class ShuffledSequenceLoader:
 	def _reset_shard(self,si):max_phase=min(self.seq_len-1,max(0,self.num_tokens[si]-self.seq_len-1));phase=int(self.rng.integers(max_phase+1))if max_phase>0 else 0;num_sequences=(self.num_tokens[si]-1-phase)//self.seq_len;sequence_order=self.rng.permutation(num_sequences);self.start_inds[si]=(phase+sequence_order*self.seq_len).tolist()
 	def next_batch(self,global_tokens,grad_accum_steps):
 		device_tokens=global_tokens//(self.world_size*grad_accum_steps);device_batch_size=device_tokens//self.seq_len;remaining=np.array([len(s)for s in self.start_inds],dtype=np.float64);x=torch.empty((device_batch_size,self.seq_len),dtype=torch.int64);y=torch.empty((device_batch_size,self.seq_len),dtype=torch.int64)
+		x_np = np.empty((device_batch_size, self.seq_len), dtype=np.int64)
+		y_np = np.empty((device_batch_size, self.seq_len), dtype=np.int64)
+
 		for bi in range(device_batch_size):
 			total=remaining.sum()
 			if total<=0:
 				for si in range(len(self.files)):self._reset_shard(si)
 				remaining=np.array([len(s)for s in self.start_inds],dtype=np.float64);total=remaining.sum()
-			probs=remaining/total;si=int(self.rng.choice(len(self.files),p=probs));start_ind=self.start_inds[si].pop();remaining[si]-=1;mm=_get_shard_memmap(self.files[si]);window=torch.from_numpy(mm[start_ind:start_ind+self.seq_len+1]);x[bi]=window[:-1];y[bi]=window[1:]
-		return x.to(self.device,non_blocking=True),y.to(self.device,non_blocking=True)
+			probs=remaining/total;si=int(self.rng.choice(len(self.files),p=probs));start_ind=self.start_inds[si].pop();remaining[si]-=1;mm=_get_shard_memmap(self.files[si]);
+			window=mm[start_ind:start_ind+self.seq_len+1]
+			x_np[bi]=window[:-1]
+			y_np[bi]=window[1:]
+		return torch.from_numpy(x_np).to(self.device,non_blocking=True),torch.from_numpy(y_np).to(self.device,non_blocking=True)
 
 class RMSNorm(nn.Module):
 	def __init__(self,eps=None):super().__init__();self.eps=eps
